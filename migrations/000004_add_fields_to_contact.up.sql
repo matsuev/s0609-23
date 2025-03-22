109d575743fd2d6fe75dd0b91c6697d239d5fcd4
Alex Matsuev <alex.matsuev@gmail.com>
2025-03-22 16:16:57 +0300

    Add migration service

--- migrations/000004_add_fields_to_contact.up.sql
+++ migrations/000004_add_fields_to_contact.up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE IF EXISTS public.contact
-   ADD COLUMN IF NOT EXISTS mother_name character variyng(50) NOT NULL DEFAULT '';
+   ADD COLUMN IF NOT EXISTS mother_name character varying(50) NOT NULL DEFAULT '';
 
 ALTER TABLE IF EXISTS public.contact
-   ADD COLUMN IF NOT EXISTS father_name character variyng(50) NOT NULL DEFAULT '';
+   ADD COLUMN IF NOT EXISTS father_name character varying(50) NOT NULL DEFAULT '';
